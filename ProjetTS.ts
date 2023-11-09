106cc21d005cb952dee753bde12ad38b0729511b
CasualCali <113681925+CasualCali@users.noreply.github.com>
2023-11-10 00:22:54 +0100

    Update ProjetTS.ts

--- ProjetTS.ts
+++ ProjetTS.ts
@@ -1,17 +1,20 @@
 import axios from 'axios';
 import * as fs from 'fs';
-import * as mammoth from 'mammoth';
 
 // Chemin d'accès des fichiers
-const googleDriveFileUrl = 'https://drive.google.com/uc?export=download&id=1wYOUPKpnObyd3FPa-l6HnRxvUqIqdKZe';
+ //const googleDriveFileUrl = 'https://drive.google.com/uc?export=download&id=1wYOUPKpnObyd3FPa-l6HnRxvUqIqdKZe';
+const localFilePath = 'C:\\Users\\DELL\\Desktop\\TypeScript\\Wordatester.docx';
 //const googleDriveFileUrl = 'https://drive.google.com/uc?export=download&id=1qB8GLHCN38LtK3m_5vDCthlixBZFoxFK';
 
 // Permet de lire le contenu d'un fichier depuis un Drive
 async function readTextFileFromGoogleDrive(url: string): Promise<string | null> {
     try {
-        const response = await axios.get(url);
+        const response = await axios.get(url, { responseType: 'arraybuffer' });
+
         if (response.status === 200) {
-            return response.data;
+            // Convertir les données binaires en chaîne de caractères
+            const text: string = Buffer.from(response.data, 'binary').toString('utf-8');
+            return text;
         } else {
             console.error('Erreur lors de la récupération du fichier depuis Google Drive.');
             return null;
@@ -26,8 +29,7 @@ async function readTextFileFromGoogleDrive(url: string): Promise<string | null> 
 async function readWordFile(filePath: string): Promise<string | null> {
     try {
         const data: Buffer = await fs.promises.readFile(filePath);
-        const result = await mammoth.extractRawText({ path: filePath });
-        const text: string = result.value; // Texte brut extrait du fichier Word
+        const text: string = data.toString();
         return text;
     } catch (err) {
         console.error('Erreur de lecture du fichier Word :', err);
@@ -39,43 +41,43 @@ async function readWordFile(filePath: string): Promise<string | null> {
 function addSpaces(sentence: string): string {
     // Ajoute un espace après chaque virgule si ce n'est pas déjà le cas
     sentence = sentence.replace(/,/g, ', ');
-
+  
     // Retire un espace après chaque virgule s'il y en a deux
     sentence = sentence.replace(/,  /g, ', ');
-
+  
     // Ajoute un espace après une minuscule ou une majuscule si le caractère suivant est '...', '!', ou '?', si ce n'est pas déjà le cas
     sentence = sentence.replace(/([a-zA-Z])([…!?])(?![ ])/g, '$1 $2');
-
+  
     // Ajoute un espace après une minuscule si elle est suivie d'un chiffre ou d'une majuscule
     sentence = sentence.replace(/([a-z])([0-9A-Z])/g, '$1 $2');
-
+  
     // Ajoute un espace après un chiffre s'il est suivi d'une minuscule dotée d'un accent ou d'une majuscule
     sentence = sentence.replace(/([0-9])([a-zA-ZÀ-ÖØ-öø-ÿ])/g, '$1 $2');
-
+  
     return sentence;
-}
-
-// Permet de séparer et afficher les phrases
-function splitAndDisplayPhrases(text: string | null): void {
+  }
+  
+  // Permet de séparer et afficher les phrases
+  function splitAndDisplayPhrases(text: string | null): void {
     if (text) {
-        // Séparez les phrases après chaque '.', '!', '?', ou '...'
+        // Permet de séparer les phrases après chaque '.', '!', '?', ou '...'
         const sentences: string[] = text.split(/(?<=[.!?…])/);
-
-        // Affichez chaque phrase avec des espaces ajoutés
+  
+        // Permet d'afficher chaque phrase avec des espaces ajoutés
         sentences.forEach((sentence, index) => {
-            // Ajoutez un espace aux phrases non vides avant de les afficher
+            // Permet d'ajouter un espace aux phrases non vides avant de les afficher
             sentence = addSpaces(sentence);
             if (sentence.trim()) {
                 console.log(`Phrase ${index + 1}: ${sentence.trim()}`);
             }
         });
     }
-}
-
-// Permet d'ajouter une majuscule au début de chaque phrase
-function capitalizeSentences(text: string | null): string {
+  }
+  
+  // Permet d'ajouter une majuscule au début de chaque phrase
+  function capitalizeSentences(text: string | null): string {
     const sentences: string[] = text.split(/(?<=[.!?…])/);
-
+  
     const capitalizedSentences: string[] = sentences.map((sentence) => {
         // Vérifie si la première lettre de la phrase est une minuscule
         if (sentence.trim() && sentence[0] === sentence[0].toLowerCase()) {
@@ -83,9 +85,9 @@ function capitalizeSentences(text: string | null): string {
         }
         return sentence;
     });
-
+  
     return capitalizedSentences.join('');
-}
+  }
 
 // Permet de lire et traiter le fichier
 async function processFile(filePath: string, isLocal: boolean = true): Promise<void> {
@@ -106,6 +108,13 @@ async function processFile(filePath: string, isLocal: boolean = true): Promise<v
     }
 }
 
-// Permet d'appeler la fonction pour lire et traiter le fichier depuis un Drive
-processFile(googleDriveFileUrl, false);
-
+// Condition pour choisir l'URL
+// if (localFilePath) {
+    // // Appeler la fonction pour lire et traiter un fichier Word
+     processFile(localFilePath, true);
+// } else if (googleDriveFileUrl) {
+    // Appeler la fonction pour lire et traiter le fichier depuis un Drive
+    //processFile(googleDriveFileUrl, false);
+// } else {
+//     console.error('Aucun chemin de fichier spécifié.');
+// }
